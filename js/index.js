7b490ab5b0279027816ce7c72a777122245a86ef
DIR-Bunny <sarwade.vaibhav@gmail.com>
2017-06-22 18:21:25 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,46 +1,7 @@
-/*
-*
-*@author: Vaibhav Sarwade
-*
-*/
-/**
- * 
- */
-
-$(document).ready(function() 
-{
-	$(".projectNames").click(function()
-	{
-		var type = $(this).find("h1").text();
-		$("#myModalLabel").text(type);
-		var AllData = $("."+type+"Pannel").html();
-		$(".myModalBody").text('');
-		$(".myModalBody").append(AllData);
-		$("#MyModal").modal('show');
-		
-	});
-	////////////All close functions
-	$('.close').click(function() 
-	{
-		$("#MyModal").modal('hide');
-	});
-	
-	// ===== Scroll to Top ==== 
-	$(window).scroll(function()
-	{
-	    if ($(this).scrollTop() >= 50) 
-	    { 
-		$('#return-to-top').fadeIn(200);    
-	    } else {
-		$('#return-to-top').fadeOut(200);   
-	    }
-	});
-	$('#return-to-top').click(function() 
-	{      
-	    $('body,html').animate({
-		scrollTop : 0                       
-	    }, 500);
-	});
-
-});
-
+$(document).ready(function()
+{$(".projectNames").click(function()
+{var type=$(this).find("h1").text();$("#myModalLabel").text(type);var AllData=$("."+type+"Pannel").html();$(".myModalBody").text('');$(".myModalBody").append(AllData);$("#MyModal").modal('show')});$('.close').click(function()
+{$("#MyModal").modal('hide')});$(window).scroll(function()
+{if($(this).scrollTop()>=50)
+{$('#return-to-top').fadeIn(200)}else{$('#return-to-top').fadeOut(200)}});$('#return-to-top').click(function()
+{$('body,html').animate({scrollTop:0},500)})})
